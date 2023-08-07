6e254830e2c0a282966e3999d1a904eff245124f
oliveirawell <wellingtonmaciel00@outlook.com>
2023-08-07 14:54:27 +0100

    From workstation

--- Study/list.py
+++ Study/list.py
@@ -17,3 +17,4 @@ Del listname[index]
 supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
