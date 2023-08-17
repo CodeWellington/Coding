6e961754afb7582751e7da9b64bdca890a7bb070
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2023-08-17 18:15:31 +0100

    Update class.py

--- Study/class.py
+++ Study/class.py
@@ -1,11 +1,18 @@
 class Router:
     '''This is a class for router'''
-    #Functions in class are called as methods
+    #Functions in class are named as methods
     def __init__(self, model, software, ip, ports):
         self.model = model
         self.software = software
         self.ip = ip
         self.ports = ports
+    def get_des(self):
+        '''Return the device information'''
+        desc = f"Model:          {self.model}\n" \
+               f"Software:       {self.software}\n" \
+               f"IP:             {self.ip}"
+        return desc
+
 
 rtr1 = Router("9300", "16.7.4", "10.10.10.10", "48")
-print(rtr1.ports)
+print(rtr1.get_des())
