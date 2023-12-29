459b8876fe3da0f7b5e692e64232dc3afda9ed9f
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2023-12-29 14:44:35 +0000

    Update gte cdp neig information.py

--- Exercises/gte cdp neig information.py
+++ Exercises/gte cdp neig information.py
@@ -4,7 +4,6 @@ import csv
 import pandas as pd
 import time
 
-
 def ssh(host):
     ssh_client = paramiko.SSHClient()  #ssh client from paramiko
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Helps when the key is unkown
@@ -16,31 +15,40 @@ def ssh(host):
     time.sleep(2)
     return session
 
-hosts = [ "100.64.0.200", "100.64.0.201", "100.64.0.202"]
-df = pd.DataFrame(columns=["Device", "Neighbor", "IP-ADDRESS", "Platform"])
-df.to_csv("D:/IT/Python/Automation/test3.csv", index=False, mode="a+")
-time.sleep(2)
-for host in hosts:
-    try:
+hosts = ["100.64.0.202", "100.64.0.201"] #List of devices
+df = pd.DataFrame(columns=["Device", "Neighbor", "IP", "Platform"]) #Header
+df.to_csv("D:/IT/Python/Automation/test3.csv", index=False, mode="a+") #Change the path as required, writing the header to csv file
+time.sleep(1)
+for host in hosts: #Looping tru the hosts
+    access = True #reseting the access to True
+    try: #first try to access the devices
         config = ssh(host)
-        config.send(b"show cdp neighbors  detail\n")
-        time.sleep(2)
-        log = str(config.recv(99999999).decode("utf-8"))
-        if "--More--" in log:
-            config.send(" ")
-            time.sleep(2)
-            log += str(config.recv(99999999).decode("utf-8"))
-        try:
+        config.send(b"show cdp neighbors  detail\n") #send the command tho host
+        time.sleep(1)
+        log = str(config.recv(99999999).decode("utf-8")) #output of the confi.send
+        while True: #when you have lots of neighbors the --More-- will show up
+            if "--More--" in log:
+                config.send(" ")
+                time.sleep(1)
+                log_temp = str(config.recv(99999999).decode("utf-8")) #output after send the space command (not the sull output)
+                log += log_temp #concatenate to keep the full output
+            if "--More--" in log_temp: #checking if it needs to send another space command
+                config.send(" ")
+                time.sleep(1)
+                log += str(config.recv(99999999).decode("utf-8"))  #concatenate to keep the full output
+            else:
+                break
+        try: #try to find the neighbor - Device ID: <host>
             neig = re.findall(r"Device ID: (\S+)", log)
             df["Neighbor"] = neig
         except ValueError:
             df["Neighbor"] = "Not found"
-        try:
-            ip = re.findall(r"Entry address\(es\): \r\n  IP address: (\d+\.\d+\.\d+\.\d+)", log, flags=re.DOTALL)
-            df["IP-ADDRESS"] = ip
+        try: #try to find the ip - Entry address(es): \r\n  IP address: <x.x.x.x>
+            ip = re.findall(r"Entry address\(es\): \r\n.+:\s(\d+\.\d+\.\d+\.\d+)", log)
+            df["IP"] = ip
         except ValueError:
-            df["IP-ADDRESS"] = "Not found"
-        try:
+            df["IP-address"] = "Not found"
+        try: #try to find the platform - Platform: <platform>,
             platform = re.findall(r"Platform: (.+),", log)
             df["Platform"] = platform
         except ValueError:
@@ -49,6 +57,10 @@ for host in hosts:
         print(f"Access to {host} OK")
     except paramiko.ssh_exception.NoValidConnectionsError:
         print(f"no Access to {host}")
+        access = False #if inaccessible
     finally:
-        df.to_csv("D:/IT/Python/Automation/test3.csv", index=False, mode="a+", header=False)
-        time.sleep(2)
+        if access: #if accessible write to csv
+            df.to_csv("D:/IT/Python/Automation/test3.csv", index=False, mode="a+", header=False)
+            time.sleep(1)
+        else: # if inaccessible just pass
+            pass
