4c797b699460d4c878f71e0021cb9931acf63971
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2023-12-30 11:54:56 +0000

    Update get cdp neig information.py

--- Exercises/get cdp neig information.py
+++ Exercises/get cdp neig information.py
@@ -20,26 +20,26 @@ df.to_csv(r"C:\path\test.csv", index=False, mode="a+") #Change the path as requi
 for host in hosts: #Looping tru the hosts
     try: #first try to access the devices
         config = ssh(host)
-        config.send(b"show cdp neighbors  detail\n") #send the command tho host
+        config.send(b"show cdp neighbors  detail\n") #send the command to host
         time.sleep(1)
         log = str(config.recv(99999999).decode("utf-8")) #output of the config.send
         df["Device"] = [re.search(r"(\S+)#", log).group(1)] #device name
         log = log.split("-------------------------") #splitting the neighbors
         for device in log: #for each neighbor
-            if "Device ID:" in device: #making sure is a neighbor
+            if "Device ID:" in device: #making sure it is a neighbor
                 try:  # try to find the neighbor - Device ID: <host>
                     df["Neighbor"] = re.search(r"Device ID: (\S+)", device).group(1)
                 except AttributeError:
                     df["Neighbor"] = "Not found"
                 try:  # try to find the ip - Entry address(es): \r\n  IP address: <x.x.x.x>
-                    df["IP"] = re.search(r"Entry address\(es\):.+IP address: (.+?)\n", device, flags=re.DOTALL).group(1)
+                    df["IP"] = re.search(r"Entry address\(es\):.+IP address: (.+?)\n", device, flags=re.DOTALL).group(1) #using the flags=re.DOTALL to include \r\n
                 except AttributeError:
                     df["IP"] = "Not found"
                 try:  # try to find the platform - Platform: <platform>,
                     df["Platform"] = re.search(r"Platform: (.+),", device).group(1)
                 except AttributeError:
                     df["Platform"] = "Not found"
-                df.to_csv(r"C:\Users\DEOLIWX2\OneDrive - Abbott\Documents\Training\Python/test6.csv", index=False, mode="a+", header=False) #writhing a row to csv
+                df.to_csv(r"C:\Users\DEOLIWX2\OneDrive - Abbott\Documents\Training\Python/test6.csv", index=False, mode="a+", header=False) #writhing a row to csv without the header
         print(f"Access to {host} OK")
     except paramiko.ssh_exception.NoValidConnectionsError:
         print(f"no Access to {host}")
