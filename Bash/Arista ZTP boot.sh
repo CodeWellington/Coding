e061956b421b50502c17a66f7c9af740726c2930
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2024-04-03 15:12:25 +0100

    Update Arista ZTP boot.sh

--- Bash/Arista ZTP boot.sh
+++ Bash/Arista ZTP boot.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+#filter by serial number
 OUTPUT=$(Cli -c "show version | inc Serial number")
 SN=$(echo $OUTPUT | awk '{print $3}')
 
-
+#replace IP with the FTP server IP
 tftp IP -c get $SN /mnt/flash/startup-config
+#You must have a configuration file named with the serial number
