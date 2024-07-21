6e23f47f794119d3f578cf3c380f0c45828acd03
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2024-07-21 19:44:41 +0100

    Create Disabling VMnet8.py

---
+++ Study/Disabling VMnet8.py
@@ -0,0 +1,14 @@
+###When doing labs, sometimes while on wireless you need to disable and re-enable the VMnet 8, which the is net intf for VMware
+
+import subprocess
+import time
+print("Disabling the VMnet8...")
+subprocess.run('netsh interface set interface "VMware Network Adapter VMnet8" admin=disabled', shell=True)
+time.sleep(4)
+subprocess.run('netsh interface show interface "VMware Network Adapter VMnet8"')
+time.sleep(1)
+print("Enabling VMnet8...")
+subprocess.run('netsh interface set interface "VMware Network Adapter VMnet8" admin=enabled', shell=True)
+time.sleep(4)
+
+##Convert this scrip to exe and run as admin
