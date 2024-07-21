4a3d11dd69e844c7aa3fb00163dafcbdd31ae698
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2024-07-21 20:15:42 +0100

    Update Disabling VMnet8.py

--- Study/Disabling VMnet8.py
+++ Study/Disabling VMnet8.py
@@ -4,11 +4,12 @@ import subprocess
 import time
 print("Disabling the VMnet8...")
 subprocess.run('netsh interface set interface "VMware Network Adapter VMnet8" admin=disabled', shell=True)
-time.sleep(4)
+time.sleep(8)
 subprocess.run('netsh interface show interface "VMware Network Adapter VMnet8"')
 time.sleep(1)
 print("Enabling VMnet8...")
 subprocess.run('netsh interface set interface "VMware Network Adapter VMnet8" admin=enabled', shell=True)
+subprocess.run('netsh interface show interface "VMware Network Adapter VMnet8"')
 time.sleep(4)
 
 ##Convert this scrip to exe and run as admin
