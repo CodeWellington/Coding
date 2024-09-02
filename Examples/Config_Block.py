5a86502da475598b3ad0975593f9918f2af4dfc1
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2024-09-02 19:53:47 +0100

    Create Config_Block.py

---
+++ Examples/Config_Block.py
@@ -0,0 +1,27 @@
+import re
+##Read a config file##
+with open('config.txt', 'r', newline='') as file:
+ lines = file.readlines()
+ lines = [line.rstrip('\n\r') for line in lines]
+
+def config_block(file, regex):
+  ##Find the block of code based on regex - Ex. ap profile <name> [ap\sprofile\s.*]
+ output = {}
+ temp = ""
+
+ for line in file:
+  if temp and line.startswith(" "): #Config block starts with space
+   output[temp].append(line)
+  elif temp and not line.startswith(" "):
+   temp = "" #When the block finishes
+
+  try:
+   temp = re.search(regex, line).group(0)
+   output[temp] = []
+  except:
+   continue
+ return output
+
+##Example
+profi_regex = 'ap\sprofile\s.*'
+profi = config_block(lines, profi_regex)
