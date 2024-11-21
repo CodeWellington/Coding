66cd0dd4e2ca502af5627bd418bfdd528f175ded
CodeWellington <141629947+CodeWellington@users.noreply.github.com>
2024-11-21 19:32:06 +0000

    Create config builder.py

---
+++ Examples/config builder.py
@@ -0,0 +1,43 @@
+def read_file():
+    # Reading the config file
+    while True:
+        file = input("Enter the Host File name to read: ")
+        try:
+            with open(file, "r", newline="") as host_file:
+                host_file.readline()  # Reading Header
+                hostnames = host_file.readlines()
+                hostnames = [line.rstrip("\r\n") for line in hostnames]  # Removing the \r\n
+                return hostnames
+        except FileNotFoundError:
+            print(f"No such file or directory: {file}")
+
+
+def config_gen(config, hostnames):
+    # Building the configuration
+    full_config = []
+    for host in hostnames:
+        full_config.append("#"*60 + "\n")
+        full_config.append("{:!^60}".format(host) + "\n")
+        full_config.append("#" * 60 + "\n")
+        for line in config:
+            full_config.append(line + "\n")
+    return full_config
+
+
+def write_file(file_name, config_file):
+    # Writing the config to a file
+    with open(file_name, "w") as write:
+        write.write("Configuration\n")
+        for line in config_file:
+            write.write(line)
+
+hosts = read_file()
+configuration = [
+    "config t",
+    "<commands>",
+    "end",
+    "write",
+    "!"]
+
+output = config_gen(configuration, hosts)
+write_file(str(input("What is the output file name: ")), output)
